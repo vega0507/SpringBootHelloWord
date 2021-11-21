9819ee50d62ed6d24780fc536980ee26b8e5cebe
VIOLE-ASUS\viole <vgaitan@derivsoft.com>
2021-11-20 23:24:32 -0600

    Agregando primer servicio de prueba

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 "# SpringBootHelloWord" 
+
+
+echo "# SpringBootHelloWord" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vega0507/SpringBootHelloWord.git
+git push -u origin main
---
+++ src/main/java/com/violeta/pruebas/Greeting.java
@@ -0,0 +1,13 @@
+package com.violeta.pruebas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Greeting {
+	@RequestMapping("/greeting")
+	public @ResponseBody String greeting(){
+		return "Hello";
+	}
+}
